cceb1a90b05acb522dcca426ca529b9140cd4991
Matt West <matt@codeslo.com>
2017-08-28 16:14:20 -0700

    added mock http service

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  providers: [HeroService]
+})
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private router: Router,private heroService: HeroService) { }
+
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  goToDetail(): void{
+    this.router.navigate(['/detail',this.selectedHero.id]);
+  }
+}
+
